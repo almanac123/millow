1331ee1b4d0a222fb70f7cd294370917419a7e5e
Anubhab Patra <anubhabpatra02@gmail.com>
2024-01-13 19:01:36 +0530

    RealEstate Blockchain

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,40 @@
-import logo from '../assets/logo.svg';
+import logo from "../assets/logo.svg";
 
 const Navigation = ({ account, setAccount }) => {
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+  };
+  return (
+    <nav>
+      <ul className="nav__links">
+        <li>
+          <a href="#"></a>
+        </li>
+        <li>
+          <a href="#"></a>
+        </li>
+        <li>
+          <a href="#"></a>
+        </li>
+      </ul>
 
-}
-
+      <div className="nav__brand">
+        <img src={logo} alt="Logo" />
+        <h2>ChainEstate</h2>
+      </div>
+      {account ? (
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
+        </button>
+      ) : (
+        <button type="button" className="nav__connect" onClick={connectHandler}>
+          Connect
+        </button>
+      )}
+    </nav>
+  );
+};
 export default Navigation;
